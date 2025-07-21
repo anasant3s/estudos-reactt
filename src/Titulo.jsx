6d54d30df3611e231b0cfce403881586e0b62e48
ana <annnasantos4530@gmail.com>
2025-07-21 16:16:32 -0300

    Add files via upload

--- src/Titulo.jsx
+++ src/Titulo.jsx
@@ -1,13 +1,15 @@
-function Titulo({nome, paragrafo, cor}){
-   
+//colocar a memoria dentro do componente
+import { useState } from "react";
+
+//altera oq tem na memoria
+function Titulo({cor}){
+    const [texto, setTexto] = useState("Um titulo do estado inicial")
     return (
         <div>
-             <h1 style={{color:cor}}> titulo, {nome ? nome : "Sem nome"}</h1>
-             {paragrafo ?
-             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In cum tempore quis ullam at modi aperiam, dolorem deleniti laudantium, fugit illum, totam minus quod eum. Debitis quos est fuga culpa.</p>
-             :
-             <p>nada</p>
-}
+             <h1 style={{color:cor}}>{texto}</h1>
+             <button onClick={()=>{setTexto("mudei via botão")}}>Mudar</button>
+           
+
         </div>
     )
 
